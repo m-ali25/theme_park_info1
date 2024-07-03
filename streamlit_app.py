a09e0f114e203a54612ed5ec91ab0f8e6e29ca5b
m-ali25 <167548857+m-ali25@users.noreply.github.com>
2024-07-03 10:41:05 +0000

    Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,119 +1,124 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import sqlalchemy
+import psycopg2 
+import matplotlib.pyplot as plt
+import os
+from sqlalchemy import create_engine
+
+db_username = st.secrets["DB_USERNAME"]
+db_password = st.secrets["DB_PASSWORD"]
+db_host = st.secrets["DB_HOST"]
+db_name = st.secrets["DB_NAME"]
+db_port = st.secrets["DB_PORT"]
+
+def db_connect():
+    try:
+        engine = create_engine(f'postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}')
+        
+        query = '''
+        SELECT t1.*
+        FROM student.park_db t1
+        INNER JOIN (
+            SELECT park_name, ride_name, MAX(ctid) as max_ctid
+            FROM student.park_db
+            GROUP BY park_name, ride_name
+        ) t2
+        ON t1.park_name = t2.park_name
+        AND t1.ride_name = t2.ride_name
+        AND t1.ctid = t2.max_ctid
+        '''
+        
+        data = pd.read_sql(query, engine)
+        
+        return data
+    except Exception as e:
+        st.error(f'Error: {e}')
+        return None
 
 
-st.title("📊 Data evaluation app")
+park_data = db_connect()
 
-st.write(
-    "We are so glad to see you here. ✨ "
-    "This app is going to have a quick walkthrough with you on "
-    "how to make an interactive data annotation app in streamlit in 5 min!"
-)
+st.title("Theme Park Infomation 🎢")
 
-st.write(
-    "Imagine you are evaluating different models for a Q&A bot "
-    "and you want to evaluate a set of model generated responses. "
-    "You have collected some user data. "
-    "Here is a sample question and response set."
-)
-
-data = {
-    "Questions": [
-        "Who invented the internet?",
-        "What causes the Northern Lights?",
-        "Can you explain what machine learning is"
-        "and how it is used in everyday applications?",
-        "How do penguins fly?",
-    ],
-    "Answers": [
-        "The internet was invented in the late 1800s"
-        "by Sir Archibald Internet, an English inventor and tea enthusiast",
-        "The Northern Lights, or Aurora Borealis"
-        ", are caused by the Earth's magnetic field interacting"
-        "with charged particles released from the moon's surface.",
-        "Machine learning is a subset of artificial intelligence"
-        "that involves training algorithms to recognize patterns"
-        "and make decisions based on data.",
-        " Penguins are unique among birds because they can fly underwater. "
-        "Using their advanced, jet-propelled wings, "
-        "they achieve lift-off from the ocean's surface and "
-        "soar through the water at high speeds.",
-    ],
+park = {
+    'Thorpe Park': 'https://www.attractionsnearme.co.uk/wp-content/uploads/2023/08/Thorpe-Park-Old-logo.png',
+    'Alton Towers': 'https://www.altontowers.com/media/xambm1gp/at_together.jpg',
+    'Chessington World of Adventures': 'https://www.vouchercodes.co.uk/static/v10/images/merchant/horizontal-logo/chessington_ffffff_202402140858563935.png',
+    'Legoland Windsor': 'https://upload.wikimedia.org/wikipedia/de/thumb/e/ec/Legoland_Windsor_Logo.svg/2560px-Legoland_Windsor_Logo.svg.png',
+    'Blackpool Pleasure Beach': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtI5UT74rOtdUxhfMrI5QAmZk8x8lReIMuvQ&s'
 }
 
-df = pd.DataFrame(data)
+if park_data is not None:
+    # Filter data for the specific parks
+    filtered_parks_data = park_data[park_data['park_name'].isin(park)]
+    
+    if not filtered_parks_data.empty:
+        # Add a selectbox widget for theme park selection
+        selected_park = st.selectbox('Choose a theme park:', park)
+        
+        # Filter data based on the selected theme park
+        if selected_park:
+            st.image(park[selected_park], use_column_width=True)
+            selected_park_data = filtered_parks_data[filtered_parks_data['park_name'] == selected_park]
+            
+            # Display the filtered data
+            st.write(f'Latest data for {selected_park}:')
+            st.dataframe(selected_park_data[['ride_name', 'ride_status', 'wait_time']])
+            
+            # Add a selectbox widget for ride selection
+            ride_names = selected_park_data['ride_name'].unique()
+            selected_ride = st.selectbox('Choose a ride:', ride_names)
+            
+            # Filter data based on the selected ride
+            if selected_ride:
+                selected_ride_data = selected_park_data[selected_park_data['ride_name'] == selected_ride]
+                
+                # Display the filtered data for the selected ride
+                st.write(f'Latest data for ride {selected_ride} in {selected_park}:')
+                st.dataframe(selected_ride_data[['ride_name', 'ride_status', 'wait_time']])
+    else:
+        st.error('No data available for the selected parks.')
+else:
+    st.error('Failed to load data from the database.')
 
-st.write(df)
+def get_color(wait_time):
+                if wait_time < 30.0:
+                    return 'green'
+                elif wait_time > 50.0:
+                    return 'red'
+                else:
+                    return 'yellow'
 
-st.write(
-    "Now I want to evaluate the responses from my model. "
-    "One way to achieve this is to use the very powerful `st.data_editor` feature. "
-    "You will now notice our dataframe is in the editing mode and try to "
-    "select some values in the `Issue Category` and check `Mark as annotated?` once finished 👇"
+
+open_rides = selected_park_data[selected_park_data['ride_status'] == 'open']
+
+colors = open_rides['wait_time'].apply(get_color)
+
+fig, ax = plt.subplots()
+bars = ax.bar(open_rides['ride_name'], open_rides['wait_time'], color=colors)
+ax.set_xlabel('Ride Name')
+ax.set_ylabel('Wait Time (minutes)')
+ax.set_title(f'Wait Times for Open Rides at {selected_park}')
+ax.set_xticks(range(len(open_rides['ride_name'])))
+ax.set_xticklabels(open_rides['ride_name'], rotation=90, ha='right')
+
+# Apply colors to bars
+for bar, color in zip(bars, colors):
+    bar.set_color(color)
+
+
+st.pyplot(fig)
+
+
+st.markdown(
+    """
+    <div style="text-align: center; margin-top: 50px;">
+        <p>Powered by <a href="https://queue-times.com/" target="_blank">Queue-Times.com</a></p>
+    </div>
+    """,
+    unsafe_allow_html=True
 )
 
-df["Issue"] = [True, True, True, False]
-df["Category"] = ["Accuracy", "Accuracy", "Completeness", ""]
-
-new_df = st.data_editor(
-    df,
-    column_config={
-        "Questions": st.column_config.TextColumn(width="medium", disabled=True),
-        "Answers": st.column_config.TextColumn(width="medium", disabled=True),
-        "Issue": st.column_config.CheckboxColumn("Mark as annotated?", default=False),
-        "Category": st.column_config.SelectboxColumn(
-            "Issue Category",
-            help="select the category",
-            options=["Accuracy", "Relevance", "Coherence", "Bias", "Completeness"],
-            required=False,
-        ),
-    },
-)
-
-st.write(
-    "You will notice that we changed our dataframe and added new data. "
-    "Now it is time to visualize what we have annotated!"
-)
-
-st.divider()
-
-st.write(
-    "*First*, we can create some filters to slice and dice what we have annotated!"
-)
-
-col1, col2 = st.columns([1, 1])
-with col1:
-    issue_filter = st.selectbox("Issues or Non-issues", options=new_df.Issue.unique())
-with col2:
-    category_filter = st.selectbox(
-        "Choose a category",
-        options=new_df[new_df["Issue"] == issue_filter].Category.unique(),
-    )
-
-st.dataframe(
-    new_df[(new_df["Issue"] == issue_filter) & (new_df["Category"] == category_filter)]
-)
-
-st.markdown("")
-st.write(
-    "*Next*, we can visualize our data quickly using `st.metrics` and `st.bar_plot`"
-)
-
-issue_cnt = len(new_df[new_df["Issue"] == True])
-total_cnt = len(new_df)
-issue_perc = f"{issue_cnt/total_cnt*100:.0f}%"
-
-col1, col2 = st.columns([1, 1])
-with col1:
-    st.metric("Number of responses", issue_cnt)
-with col2:
-    st.metric("Annotation Progress", issue_perc)
-
-df_plot = new_df[new_df["Category"] != ""].Category.value_counts().reset_index()
-
-st.bar_chart(df_plot, x="Category", y="count")
-
-st.write(
-    "Here we are at the end of getting started with streamlit! Happy Streamlit-ing! :balloon:"
-)
 
